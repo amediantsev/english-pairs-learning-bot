2405a032a3845920bd17bf0808339bb50dada0da
amediantsev <zencrazycat@gmail.com>
2023-05-12 10:56:20 +0300

    Add retries for not valid json response from GPT.

--- src/layers/main_layer/exceptions.py
+++ src/layers/main_layer/exceptions.py
@@ -2,3 +2,7 @@ class ProcessMessageError(Exception):
     def __init__(self, message=None, *args, **kwargs):
         self.message = message
         super().__init__()
+
+
+class GptResponseFormatError(Exception):
+    pass
--- src/layers/main_layer/gpt.py
+++ src/layers/main_layer/gpt.py
@@ -1,17 +1,20 @@
 import json
 import os
 from typing import List, Dict
+import re
 
+import backoff
 import openai
+
+from exceptions import GptResponseFormatError
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 openai.api_key = OPENAI_API_KEY
 
-
-class NoCredentialsError(Exception):
-    pass
+NUM_LIST_REGEX = re.compile(r"^\d+\.\s*")
 
 
+@backoff.on_exception(backoff.expo, GptResponseFormatError, max_tries=3)
 def suggest_new_pairs(learnt_pairs: List[Dict[str, str]]) -> List[tuple]:
     if not learnt_pairs:
         return []
@@ -23,11 +26,20 @@ def suggest_new_pairs(learnt_pairs: List[Dict[str, str]]) -> List[tuple]:
                 "role": "user",
                 "content": (
                     "Generate 5 words/phrases in English with translation to Ukrainian "
-                    "in json format with array of 2 string to learn for a person "
-                    f"who last learned words/phrases: : {pairs_for_prompt}."
+                    "in json format with array of 2 strings to learn for a person "
+                    f"who last learned words/phrases: {pairs_for_prompt}."
                 ),
             }
         ],
         temperature=0.25,
     )
-    return [(pair[0], pair[1]) for pair in json.loads(response["choices"][0]["message"]["content"])]
+    print(response)
+    try:
+        raw_generated_pairs = json.loads(response["choices"][0]["message"]["content"])
+    except (KeyError, IndexError, json.JSONDecodeError):
+        raise GptResponseFormatError()
+
+    return [
+        (NUM_LIST_REGEX.sub("", pair[0]), NUM_LIST_REGEX.sub("", pair[1]))
+        for pair in raw_generated_pairs  # fmt: skip
+    ]
